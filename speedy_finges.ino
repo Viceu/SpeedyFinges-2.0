3cc097f5cefef3e4a16d3bcd7d75eb85dc73296d
Viceu <66227626+Viceu@users.noreply.github.com>
2024-02-18 03:49:15 -0500

    updated documentation

--- speedy_finges.ino
+++ speedy_finges.ino
@@ -8,21 +8,25 @@
 
 Adafruit_8x16matrix matrix = Adafruit_8x16matrix();
 
+// external component related variables
 byte arbitraryCode = 97;
 byte arbitraryCode2 = 67;
 const int touchPin = 3;
 const int button = 2;
-volatile int buttonState = 0;         // variable for reading the pushbutton status
+volatile int buttonState = 0;
 volatile int touchState = 0;
+
+// internal global variables
 int speed = 555/8;
 int pause;
-
 int count;
 int score1 = 0;
 int score2 = 0;
-int activeCol = 7;      // index of currently active column
+int activeCol = 7;  // index of currently active column
 int activeRow1;
 int activeRow2;
+
+// define music map for drop speed according to Last Christmas music
 float musicMap[56] ={1.5,1,1,0.5,
                    0.5,0.5,0.5,1.5,0.5,0.5,
                    0.5,0.5,1,1.5,0.5,
@@ -59,6 +63,7 @@ byte theGrid[] = {
     B00000000
 };
 
+// define emoji for display to reflect competition result
 byte smile_bmp[] =
   { B00111100,
     B01000010,
@@ -88,35 +93,23 @@ byte  frown_bmp[] =
     B00111100 };
 
 
+// reset all variables to starting values and prepare blank screen
 void startGame(){
     count = 0;
     speed = 555/8 * musicMap[count]; // reset speed block drops
     activeRow1 = 0;
     activeRow2 = 0;
     activeCol = 7;
-    score1 = 0;  // reset level to 0
+    score1 = 0;
     score2 = 0;
     matrix.drawBitmap(0, 0, theGrid, 8, 16, LED_ON);
 
-    void setNext();                 // kick off the first drop
+    void setNext(); // kick start the first drop
     
 }
 
 
-// check if button clicked is valid -- correct answer (not early, not late)
-// later: if return true, add to score
-// no need check for missed blocks since missing does not lose score
-boolean checkValid(int currCol) {
-    if (currCol == 7) {  // hit bottom
-        return true;
-    }
-    else {  // current block not at bottom or column, either too early or too late (next block dropped)
-        return false;
-    }
-}
-
-
-// set new column drop from top of matrix
+// set new column to drop from top of matrix
 void setNext(){
     count++;
     speed = 555/8 * musicMap[count];
@@ -126,24 +119,28 @@ void setNext(){
 }
 
 
+// code references https://www.instructables.com/Pumpktris-The-Tetris-Pumpkin/ 
 void displayScores(){
+    // setup screen
     matrix.clear();
     matrix.writeDisplay();
     matrix.setTextSize(1);
-    matrix.setTextWrap(false);  // we don't want text to wrap, so it scrolls nicely
-    //  matrix.setTextColor(LED_ON);
+    matrix.setTextWrap(false);  //no text wrap to allow scrolling display
     
+    // prepare score print outs
     String pointString1=String(score1); // convert the score integer to a string
     String scoreOne=String("Points: " + pointString1);  // build a string containing the word "points" + the point value
     int scoreOneLength = 44 + (7 * pointString1.length()); // estimating the pixel width of the string
     
-    String pointString2=String(score2); // convert the score integer to a string
-    String scoreTwo=String("Points: " + pointString2);  // build a string containing the word "points" + the point value
-    int scoreTwoLength = 44 + (7 * pointString2.length()); // estimating the pixel width of the string
+    String pointString2=String(score2);
+    String scoreTwo=String("Points: " + pointString2);
+    int scoreTwoLength = 44 + (7 * pointString2.length());
 
+    // start ending music
     Serial.write(arbitraryCode2);
 
-    for (int8_t x=8; x>= -(scoreOneLength); x--) {       // start offscreen and scroll the level from right to left
+    // scroll display scores for each player
+    for (int8_t x=8; x>= -(scoreOneLength); x--) {      // start offscreen and scroll the level from right to left
         matrix.clear();
         matrix.setCursor(x,0);
         matrix.print(scoreOne);
@@ -154,19 +151,20 @@ void displayScores(){
 
         delay(100);
     }
-
 }
 
 
+// draw current pixel
 void drawActiveShape(){
     matrix.drawPixel(activeCol, activeRow1, LED_ON);
     matrix.drawPixel(activeCol, 8 + activeRow2, LED_ON);
 }
 
 
+// setup driver and breadboard, pins, and prepare input interrrupts, then start game
 void setup() {
     Serial.begin(9600); 
-    Serial.write(arbitraryCode);
+    Serial.write(arbitraryCode);    // start music
     matrix.begin(0x70);             // initialize the top matrix
     pinMode(button, INPUT);
     pinMode(touchPin, INPUT);
@@ -179,10 +177,11 @@ void setup() {
 
 
 void loop() {
+    
+    // loop through the music map
     while (count < 56) {
 
         matrix.clear();            // clear the top matrix
-        matrix.drawBitmap(0, 0, theGrid, 8, 16, LED_ON);     // draw the fixed pieces in their locations
         
         if(activeCol > 0){  
             activeCol--;
@@ -190,24 +189,16 @@ void loop() {
         else{
             setNext();
         }
-        drawActiveShape();            // draw the active piece
-
-        matrix.writeDisplay();     // update the LEDs with the new info
         
+        drawActiveShape();            // draw the active piece
+        matrix.writeDisplay();     // update the LEDs with the new info
         delay(speed);                   
 
-
-        // at top of loop: if music ends, display scores
-        // displayScores();
-        // if (score >= count/2) {
-        //     matrix.drawBitmap(0, 0, win, 8, 16, LED_ON);
-        // }
-        // else {
-        //     matrix.drawBitmap(0, 0, lose, 8, 16, LED_ON);
-        // }
     }
 
     matrix.clear();
+
+    // display emoji for winner/loser/ties
     if(score1 > score2){
       matrix.drawBitmap(0, 0, smile_bmp, 8, 8, LED_ON); 
       matrix.drawBitmap(0, 8, frown_bmp, 8, 8, LED_ON);
@@ -220,8 +211,10 @@ void loop() {
       matrix.drawBitmap(0, 0, neutral_bmp, 8, 8, LED_ON); 
       matrix.drawBitmap(0, 8, neutral_bmp, 8, 8, LED_ON); 
     }
+
     matrix.writeDisplay();
     delay(2000);
+    // show score and play ending music to end program
     displayScores();
     delay(1000);
     exit(0);
@@ -229,19 +222,19 @@ void loop() {
 }
 
 
+// once button action triggers interrupt, see if scored at right timing
 void pin_ISR() {
     buttonState = digitalRead(button);
-    int sensorValue = digitalRead(button);
     if (activeCol == 0) {
         score1++;
     }
 }
 
 
+// once touch pin action triggers interrupt, see if scored at right timing
 void pin3_ISR() {
     touchState = digitalRead(touchPin);
-    int sensorValue = digitalRead(touchPin);
-    if (sensorValue == 1 && activeCol == 0) {
+    if (activeCol == 0) {
         score2++;
     }
 }
